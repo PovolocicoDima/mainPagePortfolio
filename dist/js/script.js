1046a74b900c6eac7fde2607733aae9ba9f52c49
Dmitrii <povolocico.dima@gmail.com>
2021-07-23 15:31:43 +0300

    links fixed

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,13 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
 const menuOverlay = document.querySelector('.menu__overlay');
+const menuListItems = document.querySelectorAll('.menu__list > li');
 
+menuListItems.forEach(item => {
+  item.addEventListener('click', () => {
+    menu.classList.remove('active');
+  })
+})
 
 hamburger.addEventListener('click', () => {
   menu.classList.add('active');
